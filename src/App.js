b92e39e773eb1b7f7bf78b0d1679ab4c4e1204ec
Magistry <Magistrigusti@yandex.ru>
2024-08-06 14:49:53 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import logo from "./img/logo.png";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src={logo} alt="logo" />
       </header>
+      <nav className="nav">
+        <div><a>Profile</a></div>
+        <div><a>Message</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      <div className="background-container">
+        <div className="text-overlay">
+          <div>avatar + discription</div>
+          <div>My post
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
+
+
+
+
 
 export default App;
